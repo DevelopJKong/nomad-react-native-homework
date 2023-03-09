7ac1760391bf33d81998b17941213200b06ebbbf
cafe small house <tbaram10@naver.com>
2023-03-09 21:13:51 +0900

    11일차 , 12일차 숙제 완료

--- screens/Coins.tsx
+++ screens/Coins.tsx
@@ -62,6 +62,15 @@ const Coins = () => {
       is_active: true,
       type: "coin",
     },
+    {
+      id: "eth-ethereum",
+      name: "Ethereum",
+      symbol: "ETH",
+      rank: 2,
+      is_new: false,
+      is_active: true,
+      type: "coin",
+    },
   ];
   return (
     <Container>
--- screens/Discover.tsx
+++ screens/Discover.tsx
@@ -1,9 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components/native";
 import { FontAwesome } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { theme } from "../theme";
-import { Animated, Image, View, PanResponder } from "react-native";
+import { Animated, View, PanResponder, ActivityIndicator } from "react-native";
+import { useQuery } from "react-query";
+import { ICoin, fetchCoins } from "../api/request-method";
 
 const Container = styled.View`
   flex: 1;
@@ -17,6 +19,7 @@ const CardBox = styled.View`
   height: 70%;
   align-items: center;
   justify-content: center;
+  position: relative;
 `;
 
 const Card = styled(Animated.createAnimatedComponent(View))`
@@ -26,6 +29,7 @@ const Card = styled(Animated.createAnimatedComponent(View))`
   justify-content: center;
   align-items: center;
   border-radius: 20px;
+  position: absolute;
 `;
 
 const CoinImg = styled.Image`
@@ -60,6 +64,8 @@ const Btn = styled.TouchableOpacity`
 `;
 
 const Discover = () => {
+  const [coinData, setCoinData] = useState<ICoin[]>([]);
+  const { isLoading, data } = useQuery<ICoin[]>("coins", fetchCoins);
   // ! Values
   const scale = useRef(new Animated.Value(1)).current;
   const position = useRef(new Animated.Value(0)).current;
@@ -67,11 +73,32 @@ const Discover = () => {
     inputRange: [-250, 0, 250],
     outputRange: ["-15deg", "0deg", "15deg"],
   });
+  const secondScale = position.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.7, 1],
+    extrapolate: "clamp",
+  });
+
+  const opacity = position.interpolate({
+    inputRange: [-300, 0, 300],
+    outputRange: [1, 0.3, 1],
+    extrapolate: "clamp",
+  });
+
+  // ! State
+  const [index, setIndex] = useState(0);
+  const onDismiss = () => {
+    scale.setValue(1);
+    position.setValue(0);
+    setIndex((prev) => prev + 1);
+  };
 
   // !  Animations
   const onPressIn = Animated.spring(scale, { toValue: 0.95, useNativeDriver: true });
   const onPressOut = Animated.spring(scale, { toValue: 1, useNativeDriver: true });
   const goCenter = Animated.spring(position, { toValue: 0, useNativeDriver: true });
+  const goLeft = Animated.spring(position, { toValue: -500, tension: 5, useNativeDriver: true });
+  const goRight = Animated.spring(position, { toValue: 500, tension: 5, useNativeDriver: true });
 
   const panResponder = useRef(
     PanResponder.create({
@@ -83,9 +110,9 @@ const Discover = () => {
       onPanResponderRelease: (_, { dx }) => {
         console.log(dx);
         if (dx < -220) {
-          Animated.spring(position, { toValue: -500, useNativeDriver: true }).start();
+          goLeft.start(onDismiss);
         } else if (dx > 220) {
-          Animated.spring(position, { toValue: 500, useNativeDriver: true }).start();
+          goRight.start(onDismiss);
         } else {
           Animated.parallel([onPressOut, goCenter]).start();
         }
@@ -94,34 +121,64 @@ const Discover = () => {
   ).current;
 
   const onLike = () => {
-    console.log("onLike");
+    goLeft.start(onDismiss);
   };
 
   const onDisLike = () => {
-    console.log("onDislike");
+    goRight.start(onDismiss);
   };
   return (
     <Container>
-      <CardBox>
-        <Card
-          {...panResponder.panHandlers}
-          style={{
-            transform: [{ scale }, { translateX: position }, { rotate: rotation }],
-          }}>
-          <CoinImg source={{ uri: "https://static.coinpaprika.com/coin/btc-bitcoin/logo.png" }} />
-          <CoinName>Bitcoin</CoinName>
-        </Card>
-      </CardBox>
-      <CardBtnsBox>
-        <BtnsWrapper>
-          <Btn onPress={onLike}>
-            <FontAwesome5 name='heart-broken' size={40} color={theme.color.white} />
-          </Btn>
-          <Btn onPress={onDisLike}>
-            <FontAwesome name='heart-o' size={40} color={theme.color.white} />
-          </Btn>
-        </BtnsWrapper>
-      </CardBtnsBox>
+      {isLoading ? (
+        <ActivityIndicator size='large' />
+      ) : (
+        <>
+          <CardBox>
+            <Card
+              style={{
+                opacity,
+                transform: [{ scale: secondScale }],
+              }}>
+              {data ? (
+                <>
+                  <CoinImg source={{ uri: `https://static.coinpaprika.com/coin/${data[index + 1].id}/logo.png` }} />
+                  <CoinName>{data[index + 1].name}</CoinName>
+                </>
+              ) : (
+                <>
+                  <ActivityIndicator size='large' />
+                </>
+              )}
+            </Card>
+            <Card
+              {...panResponder.panHandlers}
+              style={{
+                transform: [{ scale }, { translateX: position }, { rotate: rotation }],
+              }}>
+              {data ? (
+                <>
+                  <CoinImg source={{ uri: `https://static.coinpaprika.com/coin/${data[index].id}/logo.png` }} />
+                  <CoinName>{data[index].name}</CoinName>
+                </>
+              ) : (
+                <>
+                  <ActivityIndicator size='large' />
+                </>
+              )}
+            </Card>
+          </CardBox>
+          <CardBtnsBox>
+            <BtnsWrapper>
+              <Btn onPress={onLike}>
+                <FontAwesome5 name='heart-broken' size={40} color={theme.color.white} />
+              </Btn>
+              <Btn onPress={onDisLike}>
+                <FontAwesome name='heart-o' size={40} color={theme.color.white} />
+              </Btn>
+            </BtnsWrapper>
+          </CardBtnsBox>
+        </>
+      )}
     </Container>
   );
 };
