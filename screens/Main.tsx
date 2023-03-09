5fa8a01a8c3e398d21aaf9e6e404e3920da6fe6f
cafe small house <tbaram10@naver.com>
2023-03-09 16:31:38 +0900

    panResponder 사용완료

--- screens/Main.tsx
+++ screens/Main.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { Animated, Dimensions, Pressable } from "react-native";
+import { Animated, PanResponder } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -14,50 +14,14 @@ const Box = styled.View`
 `;
 const AnimatedBox = Animated.createAnimatedComponent(Box);
 
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
-
 export default function Main() {
   const POSITION = useRef(
     new Animated.ValueXY({
-      x: SCREEN_WIDTH / 2 - 200,
-      y: SCREEN_HEIGHT / 2 - 100,
+      x: 0,
+      y: 0,
     }),
   ).current;
-  const initial = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 200,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false,
-  });
 
-  const first = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: SCREEN_HEIGHT / 2 - 300,
-    },
-    useNativeDriver: false,
-  });
-
-  const second = Animated.timing(POSITION, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: SCREEN_HEIGHT / 2 - 500,
-    },
-    useNativeDriver: false,
-  });
-
-  const third = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false,
-  });
-
-  const moveUp = () => {
-    Animated.loop(Animated.sequence([first, second, third, second, first, initial])).start();
-  };
   const borderRadius = POSITION.y.interpolate({
     inputRange: [-300, 300],
     outputRange: [100, 0],
@@ -66,17 +30,27 @@ export default function Main() {
     inputRange: [-300, 300],
     outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"],
   });
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx, dy }) => {
+        POSITION.setValue({
+          x: dx,
+          y: dy,
+        });
+      },
+    }),
+  ).current;
   return (
     <Container>
-      <Pressable onPress={moveUp}>
-        <AnimatedBox
-          style={{
-            borderRadius,
-            backgroundColor: bgColor,
-            transform: [...POSITION.getTranslateTransform()],
-          }}
-        />
-      </Pressable>
+      <AnimatedBox
+        {...panResponder.panHandlers}
+        style={{
+          borderRadius,
+          backgroundColor: bgColor,
+          transform: [...POSITION.getTranslateTransform()],
+        }}
+      />
     </Container>
   );
 }
